ff3a69df952b76efccf90fa440e645419e2c457d
AlejandroDS <alexmg2197@gmail.com>
2024-02-05 19:57:45 -0600

    SE DA FORMA A SIDEBAR Y SE AGREGAN FUNCIONALIDADES

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,15 +1,23 @@
+import { React, useEffect, useState } from "react";
 import './sidebar.css'
-import Logo from '../../img/logobarber.png'
+import Logo from '../../img/55f40fef78a2107cc324218c819679c8.png'
 
 const Sidebar = () => {
+    const [mostrarElementos, setMostrarElementos] = useState(true);
+
+    const toggleVisibilidad = () => {
+        setMostrarElementos(!mostrarElementos);
+    };
+
     return (
 
         <aside className="main-sidebar elevation-4">
 
-
-            <a href="/Inicio" className='brand-link'>
-                <img src={Logo} alt="logo" className='img' />
-            </a>
+            <div className='text-center'>
+                <a href="/" className='brand-link'>
+                    <img src={Logo} alt="logo" className='img' />
+                </a>
+            </div>
 
             <div className='sidebar'>
                 <nav className='mt-2'>
@@ -18,26 +26,61 @@ const Sidebar = () => {
                         role="menu"
                         data-accordion="false">
                         <li className="nav-item">
-                            <a className="nav-link" href="/"><i className="nav-icon fas fa-home"></i> <p>Active</p> </a>
+                            <a className="nav-link" href="/"><i className="nav-icon fas fa-home"></i> <p>Home</p> </a>
+                        </li>
+                        <li className="nav-item menu-open">
+                            <a className="nav-link" onClick={toggleVisibilidad}>
+                                <i class="nav-icon fa-sharp fa-solid fa-calendar-days"></i>
+                                <p>Cita
+                                    {mostrarElementos ? < i class="right fas fa-angle-left"></i> : < i class="right fas fa-angle-up"></i>}
+
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                {
+                                    mostrarElementos && (
+                                        <div className="ms-3">
+                                            <li className="nav-item ">
+                                                <a className="nav-link " href="/">
+                                                    <i class="nav-icon fa-solid fa-calendar-plus"></i> <p>Crear Cita</p>
+                                                </a>
+                                            </li>
+                                            <li className="nav-item ">
+                                                <a className="nav-link " href="/">
+                                                    <i class="nav-icon fa-solid fa-eye"></i> <p>Ver Citas</p>
+                                                </a>
+                                            </li>
+                                        </div>
+                                    )
+                                }
+                            </ul>
                         </li>
                         <li className="nav-item">
 
-                            <a className="nav-link" href="/"><i className="nav-icon fas fa-home"></i> <p>Active</p> </a>
+                            <a className="nav-link" href="/"><i class="nav-icon fa-solid fa-hand-holding-dollar"></i> <p>Cobro</p> </a>
                         </li>
                         <li className="nav-item">
 
-                            <a className="nav-link" href="/"><i className="nav-icon fas fa-home"></i> <p>Active</p> </a>
+                            <a className="nav-link" href="/"><i class="nav-icon fa-solid fa-user-tie"></i> <p>Clientes</p> </a>
                         </li>
                         <li className="nav-item">
 
-                            <a className="nav-link" href="/"><i className="nav-icon fas fa-home"></i> <p>Active</p> </a>
+                            <a className="nav-link" href="/"><i class="nav-icon fa-solid fa-scissors"></i><p>Barbero</p> </a>
+                        </li>
+                        <li className="nav-item">
+
+                            <a className="nav-link" href="/"><i class="nav-icon fa-solid fa-cart-flatbed"></i> <p>Inventario</p> </a>
+                        </li>
+                        <li className="nav-item">
+
+                            <a className="nav-link" href="/"><i class="nav-icon fa-solid fa-clipboard-list"></i> <p>Servicios</p> </a>
                         </li>
 
                     </ul>
                 </nav>
             </div>
 
-        </aside>
+        </aside >
 
     )
 }
